d0ef66b37439102e2311fd19ee92b9d013f86b86
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-09-22 18:18:06 -0700

    A day of work.

--- script.js
+++ script.js
@@ -1,54 +1,122 @@
 // Code goes here
 (function () {
   'use strict';
+  //  MainCtrl.$inject = [ 'github'];
+  angular.module('lol', [])
+    .service('GithubPersonasService', GithubPersonasService)
+    .controller('PersonaSearchController', PersonaSearchController)
+    .controller('PersonasShowController', PersonasShowController)
+    .controller('PersonaAddController', PersonaAddController)
+    .factory('githubFactory', githubFactory);
+  //    .factory('githubRepoFactory', githubRepoFactory);
 
-  var MainCtrl = function ($scope, $http, $filter) {
+  PersonaSearchController.$inject = ['GithubPersonasService', 'githubFactory'];
 
-    $scope.message = "Hallo, Big Boss!";
+  function PersonaSearchController(GithubPersonasService, githubFactory) {
+    var person = this;
 
-    var onError = function (err) {
-      $scope.error = err.message;
+
+    person.github = githubFactory;
+
+    person.username = 'museyoucoulduse';
+
+    console.log('instatiate')
+    person.onError = function (err) {
+      person.error = err.message;
       console.log(err.message);
-    }
-
-    var onReposResponse = function (res) {
-      $scope.repos = res.data;
-      console.log($scope.repos);
-    }
-
-    var onUserResponse = function (res) {
-      $scope.data = res.data;
-      console.log($scope.data);
-      $http.get($scope.data.repos_url.toString()).then(onReposResponse, onError);
-    }
-
-    var changer = false;
-    var description = function (repo) {
-      if (
-        changer == false) {
-        return true;
-        changer = true;
-      } else if (
-        changer == true) {
-        return false;
-        changer = false;
-      }
-      $scope.description = repo.description
     };
 
+    person.onUserResponse = function (data) {
+      person.user = data;
+      console.log(person.user);
+      person.github.getRepos(person.user).then(person.onReposResponse, person.onError);
+    };
 
+    person.onReposResponse = function (data) {
+      person.repos = data;
+      console.log(person.repos);
+      person.addPersona();
+    };
 
-//    // It's me!
-//    $http.get('https://api.github.com/users/museyoucoulduse')
-//      .then(onUserResponse, onError)
+    person.search = function () {
+      console.log('search');
+      person.github.getUser(person.username).then(person.onUserResponse, person.onError)
 
-    // Who else can do the job better?
-    $scope.search = function (login) {
-      $http.get('https://api.github.com/users/' + login).then(onUserResponse, onError);
+    };
+
+    person.addPersona = function () {
+      GithubPersonasService.addPerson(person.user.login, person.repos, 0)
     };
   };
 
-  MainCtrl.$inject = ['$scope', '$http', '$filter'];
-  angular.module('lol', []).controller('MainCtrl', MainCtrl);
+  PersonasShowController.$inject = ['GithubPersonasService']
+
+  function PersonasShowController() {
+    var showPersons = this
+
+    showPersons.personas = GithubPersonasService.personas;
+
+    showPersons.removePersona = function (index) {
+      GithubPersonasService.removePerson(index);
+    }
+  };
+
+  PersonaAddController.$inject = ['GithubPersonasService']
+
+  function PersonaAddController(GithubPersonasService) {
+    var personasAdder = this;
+
+    personasAdder.login = '';
+    personasAdder.repos = '';
+    personasAdder.score = 0;
+
+    personasAdder.addPersona = () => {
+      GithubPersonasService.addPerson(personasAdder.login, personasAdder.repos, personasAdder.score);
+    };
+  };
+
+  function GithubPersonasService() {
+    var service = this;
+
+    var personas = []
+
+    service.addPerson = (login, repos, score) => {
+      var persona = {
+        login: login,
+        repos: repos,
+        score: score
+      };
+      personas.push(persona);
+    };
+
+    service.getPersonas = function () {
+      console.log('reaching for people')
+      return personas;
+    };
+
+    service.removePerson = function (index) {
+      parsonas.splice(index, 1)
+    }
+    return service;
+  };
+
+  function githubFactory($http) {
+    return {
+      getRepos: function (user) {
+        return $http.get(user.repos_url)
+          .then(function (res) {
+            return res.data;
+            console.log('Retrived repo user data');
+          });
+      },
+      getUser: function (login) {
+        return $http.get('https://api.github.com/users/' + login)
+          .then(function (res) {
+            return res.data;
+            console.log('Retrieved user data');
+          });
+      }
+    }
+  }
 
 })();
