c11d6a4b4d06f1cffc7bb593f4f38ad1718c346b
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-09-25 03:52:46 -0700

    Heroku compliant?

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/', router);
 
 var port = process.env.PORT || 5000;
 //var hostname = process.env.IP || 'localhost';
-app.listen(port, hostname, function (err) {
+app.listen(port, function (err) {
   if (err) {
     console.log(err.message);
     process.exit(1);
